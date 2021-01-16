171d34834632d2427ca8fe8f75e0c8bb76308c9f
AlejandroPerezGarcia <alejandro8001@gmail.com>
2021-01-16 13:05:43 -0300

    Muestra listado

--- app/src/main/java/cl/alejandroperez/anchorbooks/MainActivity.kt
+++ app/src/main/java/cl/alejandroperez/anchorbooks/MainActivity.kt
@@ -2,10 +2,21 @@ package cl.alejandroperez.anchorbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.alejandroperez.anchorbooks.view.ListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragments, ListFragment.newInstance("", ""), "lista")
+                .commit()
+        } else {
+            supportFragmentManager.findFragmentByTag("lista")
+        }
+
     }
 }
--- app/src/main/java/cl/alejandroperez/anchorbooks/view/DetailFragment.kt
+++ app/src/main/java/cl/alejandroperez/anchorbooks/view/DetailFragment.kt
@@ -1,23 +1,26 @@
-package cl.alejandroperez.anchorbooks
+package cl.alejandroperez.anchorbooks.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import cl.alejandroperez.anchorbooks.R
+import cl.alejandroperez.anchorbooks.model.db.EntityBookDetail
+import cl.alejandroperez.anchorbooks.viewmodel.BookViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Detail.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Detail : Fragment() {
-    // TODO: Rename and change types of parameters
+
+class DetailFragment : Fragment() {
+
+    private val bookViewModel : BookViewModel by activityViewModels()
+
+    lateinit var book : EntityBookDetail
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -49,11 +52,12 @@ class Detail : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Detail().apply {
+            DetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
+
 }
